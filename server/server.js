924693ea9b6eaa00bbd004409fd5efc66e97d5cf
Pritee Gupta <priteegupta517@gmail.com>
2025-10-08 12:18:52 +0530

    initial commit

--- server/server.js
+++ server/server.js
@@ -3,7 +3,8 @@ import serverless from "serverless-http";
 import cors from "cors";
 import "dotenv/config";
 import connectDB from "../configs/db.js";
-import clerkWebhookRouter from "../controllers/clerkwebhooks.js";
+import { clerkMiddleware } from "@clerk/express";
+import clerkWebhook from "../controllers/clerkwebhooks.js";
 
 const app = express();
 
@@ -13,20 +14,15 @@ await connectDB();
 // Middlewares
 app.use(cors());
 app.use(express.json());
+app.use(clerkMiddleware);
 
 // Routes
-// Use raw body parser ONLY for webhook verification
-app.use("/api/clerk", clerkWebhookRouter);
+app.use("/api/clerk", clerkWebhook);
 
 app.get("/", (req, res) => {
 	res.send("Server is working");
 });
 
-// Local dev server
-if (process.env.NODE_ENV !== "production") {
-	const PORT = process.env.PORT || 5000;
-	app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-}
-
+// Export serverless handler
 export default app;
 export const handler = serverless(app);
